3e757f7d8b1fe240cc3e12458b425fad1ef63333
TongCt <243684591@qq.com>
2016-12-14 20:43:47 +0800

    Update sol.py

--- BFS/BFS/Minimum Height Trees/Minimum Height Trees/sol.py
+++ BFS/BFS/Minimum Height Trees/Minimum Height Trees/sol.py
@@ -1,3 +1,4 @@
+
 class Solution(object):
     def findMinHeightTrees(self, n, edges):
         """
@@ -32,8 +33,8 @@ class Solution(object):
                 
         return que
             
-        
-        
+       ##这道题很难说是BFS， 像是拓扑排序，长度最小的树一定是 长度最长的树的一半（一定要审题，一开始我还以为是连接叶子节点的点），因此只能有一个点或者
+        ##两个点。用到了python中的函数collection.defauledict()跟dict一样http://www.cnblogs.com/herbert/archive/2013/01/09/2852843.html
                     
         
     
