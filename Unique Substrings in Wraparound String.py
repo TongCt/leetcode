603db99570525e8b4f7a6fd1b782c924bf66a211
TongCt <243684591@qq.com>
2016-12-23 14:04:40 +0800

    Update Unique Substrings in Wraparound String.py

--- Unique Substrings in Wraparound String.py
+++ Unique Substrings in Wraparound String.py
@@ -1,4 +1,4 @@
-class Solution(object):
+jcclass Solution(object):
     def findSubstringInWraproundString(self, p):
         """
         :type p: str
@@ -12,7 +12,8 @@ class Solution(object):
                 count+=1;
             else:
                 count=1;
-            res[ord(p[i])-ord('a')]=max(count,res[ord(p[i])-ord('a')]);
+            res[ord(p[i])-ord('a')]=max(count,res[ord(p[i])-ord('a')]);# 有小技巧的，去掉重复出现的字母。非常的巧妙，就是把相应的字字母对应的应该加的
+            #次数记录，选择最大的次数
             
             
         for i in range(26):
